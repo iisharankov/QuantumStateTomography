adf03a0d2cc2bfecac80e17c113e86621872614c
iisharankov <ivansharankov3@gmail.com>
2020-10-28 18:44:30 -0400

    Adds function to train real and imag NN separately using tanh activation function.

--- cml.py
+++ cml.py
@@ -5,15 +5,35 @@ from keras.layers.core import Dense, Activation
 from keras.optimizers import SGD
 import keras.backend as K
 import keras.utils as U
+from keras.layers import Layer
 from keras.utils.generic_utils import get_custom_objects
 
 from qiskit.quantum_info import state_fidelity
 
 
 
-import tensorflow as tf
-# tf.compat.v1.enable_eager_execution()
-# print(tf.version)
+# Custom layer
+class MyCustomLayer(Layer):
+    def __init__(self, output_dim,  **kwargs):
+        super().__init__(**kwargs) ##
+        self.output_dim = output_dim
+        super(MyCustomLayer, self).__init__(**kwargs)
+    def build(self, input_shape):
+        self.kernel = self.add_weight(name = 'kernel', shape = (input_shape[1], self.output_dim),
+                                      initializer = 'normal', trainable = True)
+        super(MyCustomLayer, self).build(input_shape) # Be sure to call this at the end
+
+    def call(self, input_data):
+        return K.dot(input_data, self.kernel)
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0] * 1j, self.output_dim)
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({'output_dim': self.output_dim,})
+        return config
+
 
 def load_generated_data_to_text(xfilename='XData', yfilename='YData'):
     X = []
@@ -68,8 +88,6 @@ def convertBackToImg(stateVec):
 
     return outputList
 
-
-
 def custom_loss(actsvect, estsvect):
     '''
     consumes numpy array of the ML's estimates and the actual
@@ -93,37 +111,41 @@ def custom_loss(actsvect, estsvect):
 
     # fidelity = state_fidelity(estlst, actslst)
 
-
-    fidelity = K.pow(K.abs(K.dot(actsvect, K.transpose(estsvect))), 2)
-    return 1-K.sqrt(fidelity)
+    x = actsvect
+    xo = estsvect
+    # return ((1 + x) / 2 * K.log(xo)) + ((1 - x) / 2 * K.log(1 - xo))
+    return (1/2) * ((1 - xo) * K.log(1 - K.tanh(x)) + (1 + xo) * K.log(1+K.tanh(x)))
+    # fidelity = K.pow(K.abs(K.dot(actsvect, K.transpose(estsvect))), 2)
+    # return 1-K.sqrt(fidelity)
 
 def trainModel(modelName, xTrain, yTrain):
 
+    def QMSoftMax(x):
+        '''
+        consumes a list  of scores x, returns qsoftmax
+        '''
+        softmax = K.exp(x) / K.sum(K.exp(x), axis=1, keepdims=True)
+        a = K.sqrt(softmax)
+        arctan = K.cos(x)/K.sin(x)
+        # b = K.exp(np.multiply(1j, arctan))
+        # return np.multiply(a, b)
+
+        # b = K.exp(1j * arctan)
+        # b = K.sin(arctan) + (1j * K.cos(arctan))
+        return a * K.cos(arctan)  # a * b
+
     # def QMSoftMax(x):
-    #     '''
-    #     consumes a list  of scores x, returns qsoftmax
-    #     '''
     #     softmax = K.exp(x) / K.sum(K.exp(x), axis=1, keepdims=True)
     #     a = K.sqrt(softmax)
-    #     arctan = K.cos(x)/K.sin(x)
-    #     # b = K.exp(np.multiply(1j, arctan))
-    #     # return np.multiply(a, b)
+    #     phase =  K.cos(x)/K.sin(x)
+    #     # phase = np.pi*K.softsign(x) #another possibility for phase
     #
-    #     # b = K.exp(1j * arctan)
-    #     b = K.sin(arctan) + (1j * K.cos(arctan))
-    #     return a * b
-
-    def QMSoftMax(x):
-        # softmax = K.exp(x) / K.sum(K.exp(x), axis=1, keepdims=True)
-        # a = K.sqrt(softmax)
-        # phase =  K.cos(x)/K.sin(x)
-        # # phase = np.pi*K.softsign(x) #another possibility for phase
-        #
-        # real_part = a * K.cos(phase)
-        # imaginary_part = a * K.sin(phase)
-
-        return (K.sigmoid(x) * 5) - 1
-        # return (real_part, imaginary_part)
+    #     real_part = a * K.cos(phase)
+    #     imaginary_part = a * K.sin(phase)
+    #
+    #     # return (K.sigmoid(x) * 5) - 1
+    #     # return real_part
+    #     return (real_part, imaginary_part)
 
 
     # def custom_loss(y_true, y_pred):
@@ -135,8 +157,9 @@ def trainModel(modelName, xTrain, yTrain):
     model.add(Dense(12))
     model.add(Dense(12))
     model.add(Dense(8))
-    model.add(Activation('sigmoid'))
-    # model.add(Activation(QMSoftMax, name='QMSoftMax'))
+    model.add(MyCustomLayer(8))
+    # model.add(Activation('sigmoid'))
+    model.add(Activation(QMSoftMax, name='QMSoftMax'))
 
     # for 100 - score is: 0.21507123112678528
     # for 250 - Test score is: 0.48923787474632263
@@ -145,26 +168,45 @@ def trainModel(modelName, xTrain, yTrain):
     model.compile(loss=custom_loss, optimizer=sgd)
     # model.compile(loss='mean_squared_error', optimizer=sgd)
 
-    model.fit(xTrain, yTrain, verbose=2, batch_size=1, epochs=100)
+    model.fit(xTrain, yTrain, verbose=2, batch_size=1, epochs=25)
     model.save(f'{modelName}.h5')  # creates a HDF5 file 'my_model.h5'
-
+    return model
 
 def outputModel(model, xTrain, yTrain):
-    print(model.predict(xTrain)[0])
-    print(yTrain[0])
-    print(model.predict(xTrain)[1])
-    print(yTrain[1])
+    for i in range(5):
+        print(f"#{i}: Pred", model.predict(xTrain)[i])
+        print(f"#{i}: True", yTrain[i])
     # print("\n DIFFERENCE \n")
-    # inn = np.abs(yTrain - model.predict(xTrain))
-    # output = [np.round(i, 5) for i in inn]
-    # print(inn[0:4])
+    #     inn = np.abs(yTrain - model.predict(xTrain))
+    #     output = [np.round(i, 5) for i in inn]
+    #     print(inn[0:4])
 
 def modelTest(model, xTest, yTest):
 
     predict = model.predict(xTest)
-    print(np.mean(np.square(predict - yTest)))  # discrepancy  between prediction and what we expect
+    # print(np.mean(np.square(predict - yTest)))  # discrepancy between prediction and what we expect
     score = model.evaluate(xTest, yTest, verbose=0)
     print("Test score is:", score)
+
+def trainDoubleModel(modelName, xTrain, yTrain):
+
+    # The ML Model
+    model = Sequential()
+    model.add(Dense(12))
+    model.add(Dense(12))
+    model.add(Dense(8))
+    model.add(Dense(4))
+    model.add(Activation('tanh'))
+    # model.add(Activation('softsign'))
+    # model.add(Activation('identity'))
+    # model.add(Activation(SoftMax, name='SoftMax'))
+
+    sgd = SGD(lr=0.5)
+    # model.compile(loss=custom_loss, optimizer=sgd)
+    model.compile(loss='', optimizer=sgd)
+
+    model.fit(xTrain, yTrain, verbose=2, batch_size=1, epochs=50)
+    return model
 
 if __name__ == '__main__':
     modelName = 'myFirstModel'
@@ -176,13 +218,25 @@ if __name__ == '__main__':
 
     print(xTrain)
     print(yTrain)
-    convertBackToImg(Y[1])
-    trainModel(modelName, xTrain, yTrain)
-    print("ew")
-    model = load_model(f'{modelName}.h5', custom_objects={'custom_loss':custom_loss})
+    yTrainReal = [[j[0], j[2], j[4], j[6]] for j in yTrain]
+    yTrainImag = [[j[1], j[3], j[5], j[7]] for j in yTrain]
+    modelReal = trainDoubleModel(modelName, xTrain, yTrainReal)
+    modelImag = trainDoubleModel(modelName, xTrain, yTrainImag)
+    modelReal.save(f'{modelName}_Real.h5')  # creates a HDF5 file 'my_model.h5'
+    modelImag.save(f'{modelName}_Imag.h5')  # creates a HDF5 file 'my_model.h5'
+
+    # model = load_model(f'{modelName}.h5', custom_objects={'custom_loss':custom_loss, 'MyCustomLayer':MyCustomLayer})
+    modelReal = load_model(f'{modelName}_Real.h5', custom_objects={'custom_loss': custom_loss,})
+    modelImag = load_model(f'{modelName}_Imag.h5', custom_objects={'custom_loss': custom_loss,})
     print("we")
-    # outputModel(model, xTrain, yTrain)
-    modelTest(model, xTest, yTest)
+    outputModel(modelReal, xTrain, yTrainReal)
+    print('--------')
+    outputModel(modelReal, xTrain, yTrainImag)
+
+    yTestReal = [[j[0], j[2], j[4], j[6]] for j in yTest]
+    yTestImag = [[j[1], j[3], j[5], j[7]] for j in yTest]
+    modelTest(modelReal, xTest, yTestReal)
+    modelTest(modelImag, xTest, yTestImag)
 
 
 
