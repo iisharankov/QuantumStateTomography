1b4a593eb5356ee273357c0ecc41fb0c0ba72e00
Jay Soni <jbsoni@uwaterloo.ca>
2020-10-28 23:35:03 -0400

    did some data generation stuff,

--- data_gen/generate_data.py
+++ data_gen/generate_data.py
@@ -92,7 +92,7 @@ def measure_raw(state_vect, shots=1_000_000, random_seed=1):
     """
 
     state_vect.seed(random_seed)
-    data_lst= state_vect.sample_memory(shots)
+    data_lst = state_vect.sample_memory(shots)
     results = []
     for measurement in data_lst:
         results.append(np.array([int(j) for j in list(measurement)]))
@@ -123,6 +123,50 @@ def save_generated_data_to_text(X, Y, xfilename='XData', yfilename='YData'):
             # new line for each X,Y line
             a.write("\n")
             b.write("\n")
+
+
+def generate_psi_data_set(file_name, shots=5000, num_qbits=3):
+    file = open(file_name + '.txt', 'w')
+
+    for index in range(shots):
+        psi = random_state_gen(num_qbits, real_valued_state=True)
+        psi_data = [str(i) for i in psi.data]
+
+        row = '{}'.format(psi_data[0])
+        for i in psi_data[1:]:
+            row += ',' + i
+
+        row += '\n'
+        file.write(row)
+
+    file.close()
+    return
+
+
+def generate_raw_data_set(file_name, state_vect, shots=5000):
+    file = open(file_name + '.txt', 'w')
+
+    state_vect_lst = [str(i) for i in state_vect.data]
+    psi = "{}".format(state_vect_lst[0])
+    for i in state_vect_lst[1:]:
+        psi += ',' + i
+
+    psi += '\n'
+    file.write(psi)
+
+    measurements = measure_raw(state_vect, shots)
+    for x in measurements:
+        x_str_lst = [str(j) for j in x]
+        row = "{}".format(x_str_lst[0])
+
+        for x_str in x_str_lst[1:]:
+            row += ',' + x_str
+
+        row += '\n'
+        file.write(row)
+
+    file.close()
+    return
 
 
 def main():
--- qml_approach/qml_main.py
+++ qml_approach/qml_main.py
@@ -3,6 +3,8 @@ from qiskit import *
 from qiskit.quantum_info import random_statevector
 import numpy as np
 import random
+import qpu
+import copt
 
 random.seed(1)   # setting random seed to 1 for reproducibility
 random_seed = 1  # setting the random seed inside the statevector
@@ -36,6 +38,36 @@ def initialize_theta(circ_depth=10, num_qbits=2):
 
 
 def main():
+    file_lst = ["testing_data_3qbit_psi.txt", "training_data_3qbit_psi.txt", "validation_data_3qbit_psi.txt"]
+    depth = 8
+    qbits = 3
+
+    for file_name in file_lst:
+        print('running file: {}'.format(file_name))
+        new_name = file_name.split('.')[0] + '_theta.txt'
+        file = open(file_name, 'r')
+        psi_lst = file.readlines()
+        new_file = open(new_name, 'w')
+
+        for index, line in enumerate(psi_lst):
+            print('processing line #: {}'.format(index))
+            psi_vect = [complex(v) for v in line.split(',')]
+            psi = qiskit.quantum_info.Statevector(psi_vect)
+            initial_theta = initialize_theta(circ_depth=depth, num_qbits=qbits)
+            results, optimizer_data = copt.optimize_theta_scp(initial_theta, psi)  # Final result
+            optimized_theta = optimizer_data[-1]
+
+            theta_str_lst = [str(i) for i in optimized_theta]
+            row = "{}".format(theta_str_lst[0])
+            for char in theta_str_lst[1:]:
+                row += ',' + char
+
+            row += '\n'
+            new_file.write(row)
+
+        file.close()
+        new_file.close()
+
     return
 
 
