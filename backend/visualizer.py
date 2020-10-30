3ddb8862825e44df8a378a1ff982e11f38f81cd5
Kevin <11673634+KevinBacabac@users.noreply.github.com>
2020-10-29 22:38:19 -0400

    Push backend

---
+++ backend/visualizer.py
@@ -0,0 +1,10 @@
+# Turn to base64
+import base64, io
+
+
+def _export_png(plt):
+  pic_IObytes = io.BytesIO()
+  plt.set_size_inches(3, 3)  # 300 x 300 px
+  plt.savefig(pic_IObytes, format='png')
+  pic_IObytes.seek(0)
+  return base64.b64encode(pic_IObytes.read())
