baac3b3c5a66d743c04487a399acd3000e6f0cf6
Kevin <11673634+KevinBacabac@users.noreply.github.com>
2020-10-30 00:51:27 -0400

    Proof of concept

---
+++ web/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import ComponentSelector from './components/ComponentSelector';
+import Controller from './components/Controller';
+import RadialSelector from './components/RadialSelector';
+
+
+function App() {
+  return (
+    <>
+      <Controller />
+    </>
+  );
+}
+
+export default App;
