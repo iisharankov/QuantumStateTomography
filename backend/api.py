80a902c325ed94adb751407a940b89ef9794afdc
Kevin <11673634+KevinBacabac@users.noreply.github.com>
2020-10-30 12:42:58 -0400

    Lint backend

--- backend/api.py
+++ backend/api.py
@@ -6,24 +6,26 @@ from qiskit.visualization import plot_bloch_multivector
 
 import visualizer
 
+
 def show_radial(coords):
-  plt = plot_bloch_vector_spherical(coords)
-  return visualizer._export_png(plt)
+    plt = plot_bloch_vector_spherical(coords)
+    return visualizer._export_png(plt)
 
 
 def show_multivector(coords):
-  qc = qiskit.QuantumCircuit(1)
+    qc = qiskit.QuantumCircuit(1)
 
-  non_normalized = np.array([complex(*coords[0:2]), complex(*coords[2:4])])
-  # Normalize each component
-  part_normalized = np.nan_to_num(non_normalized / np.abs(non_normalized))  # nan -> 0
+    non_normalized = np.array([complex(*coords[0:2]), complex(*coords[2:4])])
+    # Normalize each component
+    part_normalized = np.nan_to_num(
+        non_normalized / np.abs(non_normalized))  # nan -> 0
 
-  # Normalize all components
-  initial_state = part_normalized / np.linalg.norm(part_normalized)
+    # Normalize all components
+    initial_state = part_normalized / np.linalg.norm(part_normalized)
 
-  qc.initialize(initial_state, 0)
+    qc.initialize(initial_state, 0)
 
-  backend = qiskit.Aer.get_backend('statevector_simulator')
-  out = qiskit.execute(qc, backend).result().get_statevector()
-  plt = plot_bloch_multivector(out)
-  return visualizer._export_png(plt)
+    backend = qiskit.Aer.get_backend('statevector_simulator')
+    out = qiskit.execute(qc, backend).result().get_statevector()
+    plt = plot_bloch_multivector(out)
+    return visualizer._export_png(plt)
